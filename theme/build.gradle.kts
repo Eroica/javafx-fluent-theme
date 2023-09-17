c50aad5d8ef65717e079ccf8e041eab8d7675201
Eroica <seba@mails.taipei>
2023-09-17 19:02:35 +0200

    theme: Add Gradle configuration for Maven package

--- theme/build.gradle.kts
+++ theme/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     `java-library`
     id("org.openjfx.javafxplugin") version "0.1.0"
+    id("maven-publish")
 }
 
 repositories {
@@ -12,4 +13,31 @@ repositories {
 javafx {
     version = "20"
     modules("javafx.controls", "javafx.fxml")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = "javafx-fluent-theme"
+            from(components["java"])
+
+            pom {
+                name.set("javafx-fluent-theme")
+                description.set("A custom theme for JavaFX following Windows 11's designs")
+                url.set("https://github.com/Eroica/javafx-fluent-theme")
+                licenses {
+                    license {
+                        name.set("zlib License")
+                        url.set("https://raw.githubusercontent.com/Eroica/javafx-fluent-theme/main/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("Eroica")
+                        name.set("Eroica")
+                    }
+                }
+            }
+        }
+    }
 }
